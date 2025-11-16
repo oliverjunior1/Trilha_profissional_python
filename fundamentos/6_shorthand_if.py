987b69eceb37ccad3c7e005b985e4119ff9b43ed
oliverjunior1 <oliver-junior@hotmail.com>
2025-11-16 18:44:11 -0300

    Update 6_shorthand_if.py

--- fundamentos/6_shorthand_if.py
+++ fundamentos/6_shorthand_if.py
@@ -2,3 +2,6 @@
 # b = 2
 # if a > b: print('a is greater than b')
 
+age = 35
+
+print("You can drive") if age >=18 and age<70 else print("You can't drive") if age>18 else print("You are a child. Can't drive")
