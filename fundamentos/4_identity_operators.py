06405b60c1f839a0a034c7f49687bbe64696a460
oliverjunior1 <oliver-junior@hotmail.com>
2025-11-16 15:43:16 -0300

    Update 4_identity_operators.py

--- fundamentos/4_identity_operators.py
+++ fundamentos/4_identity_operators.py
@@ -12,3 +12,14 @@
 #
 # print(x is not y)
 # print(x is not x)
+
+# Difference between is and ==
+
+x = [1,2,3]
+y = [1,2,3]
+
+print(x == y) # Checks if the value of both variables are equal
+print(x is y) # Checks if both variables point to the same object in memory
+
+
+
